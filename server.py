c25242397475560d14db5dff32c3870c89ddf10e
nachiketthaokar <75263249+nachiketthaokar@users.noreply.github.com>
2022-11-06 11:44:12 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,15 @@
+import socket
+from threading import Thread
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+ip_address = '127.0.0.1'
+port = 8000
+
+server.bind((ip_address, port))
+server.listen()
+
+list_of_clients = []
+nicknames = []
+
+print("Server has started...")
